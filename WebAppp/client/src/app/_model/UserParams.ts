29bafb621e036e1d2d684e965fb88757d36e2bd3
siwakorn1123 <150316488+siwakorn1123@users.noreply.github.com>
2024-01-17 12:16:30 +0700

    color

--- WebAppp/client/src/app/_model/UserParams.ts
+++ WebAppp/client/src/app/_model/UserParams.ts
@@ -5,7 +5,7 @@ export class UserParams {
     minAge = 18
     maxAge = 80
     pageNumber = 1
-    pageSize = 5
+    pageSize = 6
 
     constructor(user: User) {
         if (user.gender === 'non-binary')
